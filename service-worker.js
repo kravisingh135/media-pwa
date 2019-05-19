a6ff7113739efd479e22574df6f04635e6adce3b
kravisingh135 <kravisingh135@gmail.com>
2019-05-19 21:46:49 +0530

    code update

--- service-worker.js
+++ service-worker.js
@@ -48,7 +48,7 @@ self.addEventListener("install", function(event) {
   console.log("[Servicework] Install");
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log("[ServiceWorker] Caching app shell", cacheName);
+      console.log("[ServiceWorker] Caching app shell", filesToCache);
       return cache.addAll(filesToCache);
     })
   );
